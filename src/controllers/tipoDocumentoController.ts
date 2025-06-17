dce3c72dfc1172edaecb5fe68c0f158f13721e6e
Juan  Marquina <juanantoniomarquina@gmail.com>
2025-06-16 22:53:29 -0500

    feat: update  endpoint tipoDocumento
    se agrego respuesta generica y se agrego constantes

--- src/controllers/tipoDocumentoController.ts
+++ src/controllers/tipoDocumentoController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import * as tipoDocumentoService from "../services/tipoDocumentoService";
+import { ResponseModel } from "../shared/responseModel";
+import { STATUS_INTERNAL_SERVER_ERROR } from "../shared/constants";
 
 export const insertarTipoDocumento = async (req: Request, res: Response) => {
     console.log('tipoDocumentoController::insertarTipoDocumento');
     try {
-        // Aquí iría la lógica para insertar un tipo de documento
-        //res.status(201).json({ message: 'Tipo de documento insertado correctamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al insertar el tipo de documento' });
+        const response = await tipoDocumentoService.insertarTipoDocumento(req.body);
+        res.json(ResponseModel.success(null,response));
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json(ResponseModel.error(error.message));
     }
 }	
 
@@ -15,40 +18,44 @@ export const listarTipoDocumentos = async (req: Request, res: Response) => {
     console.log('tipoDocumentoController::listarTipoDocumentos');
     try {
         const tipoDocumentos = await tipoDocumentoService.listarTipoDocumentos();
-        res.json(tipoDocumentos);
-    } catch (error) {
-        console.log('Error al listar tipos de documentos:', error);
-        res.status(500).json({ error: 'Error al listar los tipos de documentos' });
+        res.json(ResponseModel.success(tipoDocumentos));
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json(ResponseModel.error(error.message));
     }
 }
 
 export const obtenerTipoDocumento = async (req: Request, res: Response) => {
+    console.log('tipoDocumentoController::obtenerTipoDocumento');
     try {
-        //const { id } = req.params;
-        // Aquí iría la lógica para obtener un tipo de documento por su ID
-        //const tipoDocumento = {}; // Simulación de datos
-        //res.status(200).json(tipoDocumento);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener el tipo de documento' });
+        const { id } = req.params;
+        const tipoDocumento = await tipoDocumentoService.obtenerTipoDocumento(Number(id));
+        res.json(ResponseModel.success(tipoDocumento));
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json(ResponseModel.error(error.message));
     }
 }
 
 export const modificarTipoDocumento = async (req: Request, res: Response) => {
+    console.log('tipoDocumentoController::modificarTipoDocumento');
     try {
-        //const { id } = req.params;
-        // Aquí iría la lógica para modificar un tipo de documento por su ID
-        //res.status(200).json({ message: 'Tipo de documento modificado correctamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al modificar el tipo de documento' });
+        const { id } = req.params;
+        const response = await tipoDocumentoService.modificarTipoDocumento(Number(id), req.body);
+        res.json(ResponseModel.success(null,response));
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json(ResponseModel.error(error.message));
     }
 }
 
 export const eliminarTipoDocumento = async (req: Request, res: Response) => {
     try {
-        //const { id } = req.params;
-        // Aquí iría la lógica para eliminar un tipo de documento por su ID
-        //res.status(200).json({ message: 'Tipo de documento eliminado correctamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar el tipo de documento' });
+        const { id } = req.params;
+        const response = await tipoDocumentoService.eliminarTipoDocumento(Number(id));
+        res.json(ResponseModel.success(null, response));
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json(ResponseModel.error(error.message));
     }
 }
--- src/services/tipoDocumentoService.ts
+++ src/services/tipoDocumentoService.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { TipoDocumento } from "../models/tipoDocumento";
+import { TipoDocumento } from '../models/tipoDocumento';
+import { RESPONSE_DELETE_OK, RESPONSE_INSERT_OK, RESPONSE_UPDATE_OK } from "../shared/constants";
 
 
 const prisma = new PrismaClient();
@@ -8,10 +9,67 @@ export const listarTipoDocumentos = async() =>{
     console.log('tipoDocumentoService::listarTipoDocumentos');
 
     const tipoDocumentos: TipoDocumento[] = await prisma.tipo_documentos.findMany({
+        where: {
+            estado_auditoria: '1'
+        },
         orderBy: {
             id_tipo_documento: 'asc'
         }
     });
 
     return tipoDocumentos;
+}
+
+export const obtenerTipoDocumento = async( id: number) => {
+    console.log('tipoDocumentoService::obtenerTipoDocumento');
+
+    const tipoDocumento: TipoDocumento | null = await prisma.tipo_documentos.findUnique({
+        where: {
+            id_tipo_documento: id
+        }
+    });
+
+    return tipoDocumento;
+}
+
+export const insertarTipoDocumento = async(tipoDocumento: TipoDocumento) => {
+    console.log('tipoDocumentoService::insertarTipoDocumento');
+
+    await prisma.tipo_documentos.create({
+        data: tipoDocumento
+    });
+
+    return RESPONSE_INSERT_OK;
+}
+
+export const modificarTipoDocumento = async(id: number, tipoDocumento: TipoDocumento) => {
+    console.log('tipoDocumentoService::modificarTipoDocumento');
+
+    await prisma.tipo_documentos.update({
+        where: {
+            id_tipo_documento: id
+        },
+        data: {
+            ...tipoDocumento,
+            fecha_actualizacion: new Date()
+        }
+    });
+
+    return RESPONSE_UPDATE_OK;
+}
+
+export const eliminarTipoDocumento = async (id: number) => {
+    console.log('tipoDocumentoService::eliminarTipoDocumento');
+
+    await prisma.tipo_documentos.update({
+        where: {
+            id_tipo_documento: id
+        },
+        data: {
+            estado_auditoria: '0',
+            fecha_actualizacion: new Date()
+        }
+    });
+
+    return "Eliminado satisfactoriamente";
 }
